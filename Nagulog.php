96caef2e4d28f016a6c387006d5d3d7fcdfcc766
gustavoghioldi <gustavoghioldi@gmail.com>
2015-06-20 15:21:15 -0300

    cambios sugeridos por cliente Nagulogin

--- Nagulog.php
+++ Nagulog.php
@@ -1,8 +1,7 @@
 <?php
 class Nagulog {
-
-    static function write($mensaje=null, $tipo=null){
-        if($archivo = fopen('log.txt', "a"))
+    static function write($mensaje, $tipo="ANYONE", $log_route="logs/log.txt"){
+        if($archivo = fopen($log_route, "a"))
         {
             fwrite($archivo, date("d m Y H:m:s")." - ".$tipo." - ". ($mensaje). "\n");
             fclose($archivo);
